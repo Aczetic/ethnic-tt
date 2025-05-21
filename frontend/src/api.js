c50a08420bd48d082e23ae191f552aae3d0f2abe
Ananya <justdiscussanything@gmail.com>
2025-05-21 09:39:11 +0530

    dfd

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://ethnic-tt.onrender.com/";
 export async function fetchAllProducts(filters = {}) {
   const query = new URLSearchParams(filters).toString();
   const response = await fetch(`${BASE_URL}/customers/products?${query}`);
